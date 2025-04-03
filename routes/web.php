90dc66daf284e85210e24f064ba012bf6d91c1bb
Shaun Menezes <shaun.menezes1724@gmail.com>
2025-04-03 18:29:44 +0530

    view route logic

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotesController;
 
-// Route::get('/', [NotesController::class, 'index'])->name('index');
-
-// Route::post('/new-note', [NotesController::class, 'addNote'])->name('add-note');
-
-// Route::get('/remove-note/{id}', [NotesController::class, 'removeNote'])->name('delete-note');
-
-// Route::get('update-note/{id}', [NotesController::class, 'updateRoute'])->name('update-route');
-
-// Route::put('update-note/{id}', [NotesController::class, 'updateNote'])->name('update-note');
-
 Route::controller(NotesController::class)->group(function () {
     Route::get('/', 'index')->name('index');
-    Route::get('/new-note', 'addNoteRoute')->name('add-note-route');
-    Route::post('/new-note', 'addNote')->name('add-note');
+    Route::view('/new-note', 'create')->name('add-note-route');
+    Route::post('/add-note', 'addNote')->name('add-note');
     Route::get('/remove-note/{id}', 'removeNote')->name('delete-note');
     Route::get('update-note/{id}', 'updateRoute')->name('update-route');
     Route::put('update-note/{id}', 'updateNote')->name('update-note');
